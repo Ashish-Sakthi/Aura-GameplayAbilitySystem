2d84538b880bf1ef3227ac762d3f5cd96ae03d7b
Ashish <iamashishsakthi@gmail.com>
2025-08-04 17:43:53 +0530

    Additional bug fixes where damage is applied on client and also client side bug fixes.

--- Source/Aura/Private/AbilitySystem/Abilities/AuraBeamSpell.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraBeamSpell.cpp
@@ -12,11 +12,13 @@ void UAuraBeamSpell::StoreMouseDataInfo(const FHitResult& HitResult)
 {
 	if (HitResult.bBlockingHit)
 	{
+		// Store hit location and actor if there was a blocking hit
 		MouseHitLocation = HitResult.ImpactPoint;
 		MouseHitActor = HitResult.GetActor();
 	}
 	else
 	{
+		// Cancel ability if no hit detected
 		CancelAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true);
 	}
 }
@@ -25,6 +27,7 @@ void UAuraBeamSpell::StoreOwnerVariables()
 {
 	if (CurrentActorInfo)
 	{
+		// Cache owner's controller and character references
 		OwnerPlayerController = CurrentActorInfo->PlayerController.Get();
 		OwnerCharacter = Cast<ACharacter>(CurrentActorInfo->AvatarActor);
 	}
@@ -37,10 +40,13 @@ void UAuraBeamSpell::TraceFirstTarget(const FVector& BeamTargetLocation)
 	{
 		if (USkeletalMeshComponent* Weapon = ICombatInterface::Execute_GetWeapon(OwnerCharacter))
 		{
+			// Setup trace parameters
 			TArray<AActor*> ActorsToIgnore;
 			ActorsToIgnore.Add(OwnerCharacter);
 			FHitResult HitResult;
 			const FVector SocketLocation = Weapon->GetSocketLocation(FName("TipSocket"));
+			
+			// Perform sphere trace from weapon tip to target
 			UKismetSystemLibrary::SphereTraceSingle(
 				OwnerCharacter,
 				SocketLocation,
@@ -55,12 +61,14 @@ void UAuraBeamSpell::TraceFirstTarget(const FVector& BeamTargetLocation)
 
 			if (HitResult.bBlockingHit)
 			{
+				// Store hit information if trace successful
 				MouseHitLocation = HitResult.ImpactPoint;
 				MouseHitActor = HitResult.GetActor();
 			}
 		}
 	}
 
+	// Bind to primary target's death delegate
 	if (ICombatInterface* CombatInterface = Cast<ICombatInterface>(MouseHitActor))
 	{
 		if (!CombatInterface->GetOnDeathDelegate().IsAlreadyBound(this, &UAuraBeamSpell::PrimaryTargetDied))
@@ -72,10 +80,12 @@ void UAuraBeamSpell::TraceFirstTarget(const FVector& BeamTargetLocation)
 
 void UAuraBeamSpell::StoreAdditionalTargets(TArray<AActor*>& OutAdditionalTargets)
 {
+	// Setup actors to exclude from targeting
 	TArray<AActor*> ActorsToIgnore;
 	ActorsToIgnore.Add(GetAvatarActorFromActorInfo());
 	ActorsToIgnore.Add(MouseHitActor);
 	
+	// Find nearby valid targets
 	TArray<AActor*> OverlappingActors;
 	UAuraAbilitySystemLibrary::GetLivePlayersWithinRadius(
 		GetAvatarActorFromActorInfo(),
@@ -84,15 +94,18 @@ void UAuraBeamSpell::StoreAdditionalTargets(TArray<AActor*>& OutAdditionalTarget
 		850.f,
 		MouseHitActor->GetActorLocation());
 	
+	// Calculate number of chain targets based on ability level
 	int32 NumAdditionalTargets = FMath::Min(GetAbilityLevel() - 1, MaxNumShockTargets);
 	//int32 NumAdditionalTargets = 5;
 	
+	// Get closest targets within range
 	UAuraAbilitySystemLibrary::GetClosestTargets(
 		NumAdditionalTargets,
 		OverlappingActors,
 		OutAdditionalTargets,
 		MouseHitActor->GetActorLocation());
 
+	// Bind death delegates for additional targets
 	for (AActor* Target : OutAdditionalTargets)
 	{
 		if (ICombatInterface* CombatInterface = Cast<ICombatInterface>(Target))

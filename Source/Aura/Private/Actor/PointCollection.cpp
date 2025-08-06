aebc833cf196b884b9af2afc649f94decb151409
Ashish <iamashishsakthi@gmail.com>
2025-08-06 17:25:08 +0530

    Setup arcane shard ability activation and points collection algorithm to spawn the shards in proper location.

---
+++ Source/Aura/Private/Actor/PointCollection.cpp
@@ -0,0 +1,121 @@
+// Copyright Ashish
+
+
+#include "Actor/PointCollection.h"
+#include "AbilitySystem/AuraAbilitySystemLibrary.h"
+#include "Kismet/KismetMathLibrary.h"
+
+// Sets default values
+APointCollection::APointCollection()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	Pt_0 = CreateDefaultSubobject<USceneComponent>("Pt_0");
+	ImmutablePts.Add(Pt_0);
+	SetRootComponent(Pt_0);
+
+	Pt_1 = CreateDefaultSubobject<USceneComponent>("Pt_1");
+	ImmutablePts.Add(Pt_1);
+	Pt_1->SetupAttachment(GetRootComponent());
+
+	Pt_2 = CreateDefaultSubobject<USceneComponent>("Pt_2");
+	ImmutablePts.Add(Pt_2);
+	Pt_2->SetupAttachment(GetRootComponent());
+
+	Pt_3 = CreateDefaultSubobject<USceneComponent>("Pt_3");
+	ImmutablePts.Add(Pt_3);
+	Pt_3->SetupAttachment(GetRootComponent());
+	
+	Pt_4 = CreateDefaultSubobject<USceneComponent>("Pt_4");
+	ImmutablePts.Add(Pt_4);
+	Pt_4->SetupAttachment(GetRootComponent());
+
+	Pt_5 = CreateDefaultSubobject<USceneComponent>("Pt_5");
+	ImmutablePts.Add(Pt_5);
+	Pt_5->SetupAttachment(GetRootComponent());
+	
+	Pt_6 = CreateDefaultSubobject<USceneComponent>("Pt_6");
+	ImmutablePts.Add(Pt_6);
+	Pt_6->SetupAttachment(GetRootComponent());
+
+	Pt_7 = CreateDefaultSubobject<USceneComponent>("Pt_7");
+	ImmutablePts.Add(Pt_7);
+	Pt_7->SetupAttachment(GetRootComponent());
+
+	Pt_8 = CreateDefaultSubobject<USceneComponent>("Pt_8");
+	ImmutablePts.Add(Pt_8);
+	Pt_8->SetupAttachment(GetRootComponent());
+
+	Pt_9 = CreateDefaultSubobject<USceneComponent>("Pt_9");
+	ImmutablePts.Add(Pt_9);
+	Pt_9->SetupAttachment(GetRootComponent());
+
+	Pt_10 = CreateDefaultSubobject<USceneComponent>("Pt_10");
+	ImmutablePts.Add(Pt_10);
+	Pt_10->SetupAttachment(GetRootComponent());
+
+	
+}
+
+TArray<USceneComponent*> APointCollection::GetGroundPoints(const FVector& GroundLocation, int32 NumPoints, float YawOverride)
+{
+	// Safety check to ensure we're not requesting more points than we have available
+	checkf(ImmutablePts.Num() >= NumPoints, TEXT("Attempted to access ImmutablePts out of bounds."));
+
+	// Array to store our processed scene components that will be returned
+	TArray<USceneComponent*> ArrayCopy;
+
+	// Process each point in our immutable points collection
+	for (USceneComponent* Pt : ImmutablePts)
+	{
+		// Once we've collected the requested number of points, return the array
+		if (ArrayCopy.Num() >= NumPoints) return ArrayCopy;
+
+		// For all points except Pt_0 (our root/center point):
+		// 1. Calculate vector from root to current point
+		// 2. Rotate this vector by YawOverride degrees around the up axis
+		// 3. Set the point's new position relative to root
+		if (Pt != Pt_0)
+		{
+			FVector ToPoint = Pt->GetComponentLocation() - Pt_0->GetComponentLocation();
+			ToPoint = ToPoint.RotateAngleAxis(YawOverride, FVector::UpVector);
+			Pt->SetWorldLocation(Pt_0->GetComponentLocation() + ToPoint);
+		}
+
+		// Create trace points 500 units directly above and below the current point location
+		// These will be used to find the ground position below each point
+		const FVector RaisedLocation = FVector(Pt->GetComponentLocation().X, Pt->GetComponentLocation().Y, Pt->GetComponentLocation().Z + 500.f);
+		const FVector LoweredLocation = FVector(Pt->GetComponentLocation().X, Pt->GetComponentLocation().Y, Pt->GetComponentLocation().Z - 500.f);
+
+		// Setup variables for our line trace
+		FHitResult HitResult;
+		TArray<AActor*> IgnoreActors;
+		// Find all living players within 1500 units to ignore in our trace
+		// This prevents the trace from hitting players when finding ground
+		UAuraAbilitySystemLibrary::GetLivePlayersWithinRadius(this, IgnoreActors, TArray<AActor*>(), 1500.f, GetActorLocation());
+
+		// Configure trace parameters to ignore the players we found
+		FCollisionQueryParams QueryParams;
+		QueryParams.AddIgnoredActors(IgnoreActors);
+		// Perform line trace from raised point to lowered point, detecting any blocking geometry
+		GetWorld()->LineTraceSingleByProfile(HitResult, RaisedLocation, LoweredLocation, FName("BlockAll"), QueryParams);
+
+		// Create a new location at the ground hit point, maintaining X/Y of the original position
+		const FVector AdjustedLocation = FVector(Pt->GetComponentLocation().X, Pt->GetComponentLocation().Y, HitResult.ImpactPoint.Z);
+		// Update point position to ground location
+		Pt->SetWorldLocation(AdjustedLocation);
+		// Rotate point to align with ground surface normal
+		Pt->SetWorldRotation(UKismetMathLibrary::MakeRotFromZ(HitResult.ImpactNormal));
+
+		// Add a processed point to our return array
+		ArrayCopy.Add(Pt);
+	}
+	return ArrayCopy;
+}
+
+void APointCollection::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+

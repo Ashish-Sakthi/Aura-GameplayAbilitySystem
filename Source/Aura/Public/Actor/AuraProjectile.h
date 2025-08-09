f384543cecf2afb2b9a5d6a259fb58a38f1e7719
Ashish <iamashishsakthi@gmail.com>
2025-08-09 12:27:20 +0530

    Made projectile come back to the player using timeline and also added damage, cooldown and cost.

--- Source/Aura/Public/Actor/AuraProjectile.h
+++ Source/Aura/Public/Actor/AuraProjectile.h
@@ -40,13 +40,17 @@ public:
 protected:
 	virtual void BeginPlay() override;
 	virtual void Destroyed() override;
+	
+	UFUNCTION(BlueprintCallable)
 	void OnHit();
 
 	UFUNCTION()
-	void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	bool IsValidOverlap(AActor* OtherActor);
+	bool bHit = false;
 	
 private:
-	bool bHit = false;
 	float LifeSpan = 8.f;
 	
 	UPROPERTY(VisibleAnywhere)
